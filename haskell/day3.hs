fcf90be1913686a4f99e6ae9fd0be7b9779ae053
Nick Maher <nick.maher@gmail.com>
2023-12-04 11:53:10 +1100

    Updated parsing code so that the array is indexed (x,y) instead of (y,x)

--- haskell/day3.hs
+++ haskell/day3.hs
@@ -13,23 +13,24 @@ parse :: [String] -> EngineMap
 parse lines =
   let width = length (head lines) in
   let height = length lines in
-  let coords = [(y,x) | y <- [1..height], x <- [1..width]] in
+  let coords = [(x,y) | y <- [1..height], x <- [1..width]] in
   let cells = concat lines in 
   array ((1,1), (height, width)) (zip coords cells)
   
 neighbours :: EngineMap -> NumberDef -> [Coord]
-neighbours m ((y, x), num) =
+neighbours m ((x, y), num) =
   let len = length num in
-  let numBounds = ((y, x), (y, x+len-1)) in
-  [(ny, nx) | nx <- [x-1..x+len],
+  let numBounds = ((x, y), (x+len-1, y)) in
+  [(nx, ny) | nx <- [x-1..x+len],
               ny <- [y-1..y+1],
-              inRange (bounds m) (ny, nx),
-              not (inRange numBounds (ny, nx))]
+              inRange (bounds m) (nx, ny),
+              not (inRange numBounds (nx, ny))]
   
 numbers :: EngineMap -> [NumberDef]
 numbers m = map combine . groupBy' cmp $ digits 
-  where digits = [(i, d) | i <- indices m, let d = m ! i, isDigit d]
-        cmp ((y1,x1),_) ((y2,x2),_) = y1 == y2 && x2 == x1 + 1
+  where ix = sortOn (\(a, b) -> (b, a)) (indices m)
+        digits = [(i, d) | i <- ix, let d = m ! i, isDigit d]
+        cmp ((x1,y1),_) ((x2,y2),_) = y1 == y2 && x2 == x1 + 1
         combine ds = (fst . head $ ds, map snd ds)  
   
 isEnginePart :: EngineMap -> NumberDef -> Bool
