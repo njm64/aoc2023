01a444e78c3bb738775e1ec443d9f9642764d039
Nick Maher <nick.maher@gmail.com>
2023-12-15 10:19:56 +1100

    Added memoisation. Part 2 now completes in a couple of seconds.

--- haskell/day12.hs
+++ haskell/day12.hs
@@ -1,9 +1,8 @@
 module Day12 where
 import Data.List
-import Data.Array
-import Data.Maybe
 import Util
 import qualified Aoc
+import qualified Data.Map as Map
 
 type SpringRow = (String, [Int])
 
@@ -15,10 +14,12 @@ parseLine s =
 parse :: [String] -> [SpringRow]
 parse = map parseLine
 
-expandRecord :: SpringRow -> SpringRow
-expandRecord (s, ns) =
+expandRow :: SpringRow -> SpringRow
+expandRow (s, ns) =
   (intercalate "?" $ replicate 5 s, concat $ replicate 5 ns)
 
+-- Attempt to remove n broken springs from the beginning of xs.
+-- This will fail if the next spring is unbroken.
 removeBroken :: String -> Int -> Maybe String
 removeBroken xs n =
   let (broken, rest) = splitAt n xs in
@@ -28,17 +29,19 @@ removeBroken xs n =
     ('#':_)  -> Nothing
     (_:xs) -> Just xs
 
--- TODO: This is a work in progress. It works, but too slowly for part 2.
-  
+-- Count the number of permutations of spring arrangements
+-- Original unmemoised implementation.
+-- This was fine for part 1 but too slow for part 2.
 count :: String -> [Int] -> Int
 -- Normal termination condition
-count [] [] = 1
-count [] (_:_) = 0
--- No integers left, but there is a broken spring. Not ok.
-count ('#':_) [] = 0
--- If the first char is a dot, we can ignore it.
-count ('.':xs) ns = count xs ns
--- If the first char is a ? and no integers left, treat it like a dot
+count [] [] = 1 
+-- No broken springs left, but there is a broken count
+count [] _ = 0  
+-- No broken counts left, but there is a broken spring
+count ('#':_) [] = 0 
+-- Skip unbroken springs
+count ('.':xs) ns = count xs ns 
+-- No broken counts left, and first spring is a wildcard.
 count ('?':xs) [] = count xs []
 -- If the first character is a #, check to see if there are n
 -- consecutive broken springs followed by an unbroken spring. If
@@ -57,6 +60,27 @@ count ('?':xs) (n:ns) =
     Just xs' -> count xs (n:ns) + count xs' ns
     Nothing -> count xs (n:ns)
 
-part1 = sum . map (uncurry count)
-part2 = sum . map (uncurry count . expandRecord) 
+-- Memoised version of count. Build a map with all
+-- permutations of the tails of xs and ns. Then rely on
+-- lazy evaluation to calculate the map values as required.
+countMemo :: SpringRow -> Int
+countMemo (xs, ns) = lookup xs ns
+  where cache = Map.fromList [((xs', ns'), f xs' ns') | xs' <- tails xs, ns' <- tails ns] 
+        lookup a b = cache Map.! (a, b)
+        f [] [] = 1
+        f [] (_:_) = 0
+        f ('#':_) [] = 0
+        f ('.':xs) ns = lookup xs ns
+        f ('?':xs) [] = lookup xs []
+        f ('#':xs) (n:ns) =
+          case removeBroken ('#':xs) n of
+            Just xs -> lookup xs ns
+            Nothing -> 0
+        f ('?':xs) (n:ns) =
+          case removeBroken ('?':xs) n of
+            Just xs' -> lookup xs (n:ns) + lookup xs' ns
+            Nothing -> lookup xs (n:ns)
+
+part1 = sum . map countMemo 
+part2 = sum . map (countMemo . expandRow)
 run = Aoc.run 12 parse part1 part2
